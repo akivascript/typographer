72b97c61d88cd433f0408eb34dd38d506178a421
Akiva <akiva@akivamail.com>
2016-08-07 09:22:22 -0500

    Update Clojure and bump version.

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject typographer "1.1.0"
+(defproject typographer "1.1.1"
   :description "Converts standard ASCII punctuation into typographic smart characters."
   :url "https://github.com/akivaschoen/typographer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :source-paths ["src/clj" "src/cljs"]
   :clojurescript? true
   :cljsbuild
-  {:builds {:main 
+  {:builds {:main
             {:source-paths ["src/cljs"]
              :jar true
              :compiler {:output-to "js/typographer.js"
